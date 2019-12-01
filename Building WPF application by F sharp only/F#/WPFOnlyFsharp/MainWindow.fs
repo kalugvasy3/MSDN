bf2a7271d9da1f9067ae2bc1718c21fb9a75e6f4
kalugvasy3 <58394907+kalugvasy3@users.noreply.github.com>
2019-12-01 10:19:09 -0600

    Building WPF application by F sharp (F#) only

---
+++ Building WPF application by F sharp only/F#/WPFOnlyFsharp/MainWindow.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open WPFOnlyFsharp.Utilities
+
+// Very simple WPF ...
+//let a = Application()
+//let w = Window()
+//
+//[<STAThread>]
+//do a.Run(w) |> ignore
+
+
+let runWPFOnlyFsharp() =
+    let resource = new Uri("/WPFOnlyFsharp;component/MainWindow.fs.xaml",System.UriKind.Relative)
+    let runWindow = Application.LoadComponent(resource) :?> Window   // Cast to Window 
+    let txtHello : TextBlock = runWindow?txtHello   // Find txtHello name in runWindow and Cast to TextBlock
+    
+    let btnHello : Button = runWindow?btnHello      // Find btnHelp name in runWindow and cast to Button
+    do btnHello.Click.Add(fun _ -> do if txtHello.Text = "***" then txtHello.Text <- "Hello!" else txtHello.Text <- "***" )
+
+    runWindow   // Where "runWindow" is return value for function "runWPFOnlyFsharp()"
+
+[<STAThread>]
+(new Application()).Run(runWPFOnlyFsharp()) |> ignore
+
+
+
